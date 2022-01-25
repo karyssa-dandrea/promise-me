ae329d144caabcbb61fffb9be172eed64bb38622
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-25 11:16:58 -0800

    did my async/await and .then function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import React from 'react';
+import { getFilms } from './services/promise-me';
+import { fetchFilms } from './services/promise-me';
+
 export default function App() {
+  console.log(getFilms());
+  console.log(fetchFilms());
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,3 +1,17 @@
+import { response } from 'msw';
+
+export async function getFilms() {
+  const resp = await fetch('https://ghibliapi.herokuapp.com/films');
+  const data = await resp.json();
+  return data;
+}
+
+export const fetchFilms = () => {
+  const response = fetch('https://ghibliapi.herokuapp.com/films').then((data) =>
+    data.json()
+  );
+  return response;
+};
 /**
  * TODO:
  */
