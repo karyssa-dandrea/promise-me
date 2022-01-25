8a4590afee46dc421050c3aa6c7467400aa7418d
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-25 11:52:11 -0800

    got two api keys with console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { getFilms } from './services/promise-me';
+import React, { useEffect } from 'react';
+import { fetchBrews, getBrews, getFilms } from './services/promise-me';
 import { fetchFilms } from './services/promise-me';
 
 export default function App() {
-  console.log(getFilms());
-  console.log(fetchFilms());
+  useEffect(() => {
+    fetchFilms();
+  });
+
+  useEffect(() => {
+    fetchBrews();
+  });
+
+  useEffect(() => {
+    getBrews();
+  });
+
+  useEffect(() => {
+    getFilms();
+  });
+
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -3,47 +3,25 @@ import { response } from 'msw';
 export async function getFilms() {
   const resp = await fetch('https://ghibliapi.herokuapp.com/films');
   const data = await resp.json();
-  return data;
+  return console.log(data);
 }
 
 export const fetchFilms = () => {
-  const response = fetch('https://ghibliapi.herokuapp.com/films').then((data) =>
-    data.json()
-  );
+  const response = fetch('https://ghibliapi.herokuapp.com/films')
+    .then((data) => data.json())
+    .then((films) => console.log(films));
   return response;
 };
-/**
- * TODO:
- */
 
-/**
- * TODO:
- */
+export async function getBrews() {
+  const resp = await fetch('https://api.openbrewerydb.org/breweries');
+  const data = await resp.json();
+  return console.log(data);
+}
 
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
+export const fetchBrews = () => {
+  const response = fetch('https://api.openbrewerydb.org/breweries')
+    .then((data) => data.json())
+    .then((breweries) => console.log(breweries));
+  return response;
+};
